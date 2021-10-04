60bec7c0ca190c369a96bf09b59b711a96bd8ea8
Laura Moldovan <laura.duduma@gmail.com>
2021-10-05 01:02:24 +0200

    6

--- java/script.js
+++ java/script.js
@@ -1,127 +1,45 @@
-// Class Creation the Cards
-class Film {
-  title;
-  description;
-  year;
-  like;
-  image;
-  id;
-  lid;
+var film = JSON.parse(film);
 
-  constructor(a, b, c, d, e, f, g) {
-    this.title = a;
-    this.description = b;
-    this.year = c;
-    this.like = d;
-    this.image = e;
-    this.id = f;
-    this.lid = g;
-  }
-
-  printInfo() {
-    return `<div class="card">
-          <div class="row g-0">
-            <div class="col">
-              <img src="${this.image}" class="img-fluid rounded-start" alt= ${this.title} ${this.year} / >
-            </div>
-            <div class="col">
-              <div class="card-body">
-                <h5 class="card-title">${this.title}</h5>
-                <p class="card-text">${this.description}</p>
-              </div>
-              <div class="right">
-              <p id="${this.id}" class="btn btn-primary"> <span>&#128077</span></p>
-              <p id="${this.lid}">${this.like} Likes</p>
-              </div>
-            </div>
-          </div>
-        </div>`;
-  }
-}
-
-//Array Creation the Cards
-
-var film1 = new Film(
-    "Dune",
-    "An 2021 American epic space opera film directed by Denis Villeneuve.",
-    "2021",
-    0,
-    "img/dune.jpg",
-    0,
-    5
-  ),
-  film2 = new Film(
-    "Free Guy",
-    "An American SF action comedy film directed by Shawn Levy.",
-    "2021",
-    0,
-    "img/freeguy.jpg",
-    1,
-    6
-  ),
-  film3 = new Film(
-    "Cry Macho",
-    "An American neo-Western drama film directed and produced by Clint Eastwood.",
-    "2021",
-    0,
-    "img/crymacho.jpg",
-    2,
-    7
-  ),
-  film4 = new Film(
-    "Malignant",
-    "An American horror film directed by James Wan from a screenplay by Akela Cooper.",
-    "2021",
-    0,
-    "img/malignant.jpg",
-    3,
-    8
-  ),
-  film5 = new Film(
-    "Old",
-    "An American thriller film written, directed, and produced by M. Night Shyamalan.",
-    "2021",
-    0,
-    "img/old.jpg",
-    4,
-    9
-  ),
-  film6 = new Film(
-    "Finch",
-    "An upcoming American post-apocalyptic SF drama film directed by Miguel Sapochnik.",
-    "2021",
-    0,
-    "img/finch.jpg",
-    5,
-    10
-  );
-
-film = [film1, film2, film3, film4, film5, film6];
-
-//Print the Cards in the Dokument
-
-for (let val of film) {
-  document.getElementById("filme").innerHTML += val.printInfo();
-}
-
-//Function Increase Likes (doesn`t work yet)
-
-function likePlus(x) {
+/*function likePlus(x) {
   film[x].like += 1;
-  return document.getElementById(x+5) = film[x].like + " Likes";
+  document.getElementById(x+5) = film[x].like + " Likes";
 }
 
 //Print the Likes (doesn`t work yet)
 
-for (let i = 0; i < film.lenght; i++) {
-  let a = document.getElementById(i);
-  a.addEventListener("click", likePlus(i), false);
+for (let y of film) {
+  document.getElementById(y.id).addEventListener("click", likePlus(y.id), false);
+}*/
+
+
+for (let x of film) {
+  document.getElementById("filme").innerHTML += `<div class="card">
+  <div class="row g-0">
+    <div class="col">
+      <img src="${x.image}" class="img-fluid rounded-start" alt= ${x.title} ${x.year} / >
+    </div>
+    <div class="col">
+      <div class="card-body">
+        <h5 class="card-title">${x.title}</h5>
+        <p class="card-text">${x.description}</p>
+      </div>
+      <div class="right">
+      <p id="${x.id}" class="btn btn-primary"> <span>&#128077</span></p>
+      <p id="${x.lid}">${x.like} Likes</p>
+      </div>
+    </div>
+  </div>
+</div>`;
 }
+
+//Function Increase Likes (doesn`t work yet)
+
+
 
 //Sort the Cards (doesn`t work yet)
 
 //By Likes
-function sortFilm() {
+/*function sortFilm() {
 let filmSort = film.sort(film.like);
 for (let val of filmSort) {
   document.getElementById("filme").innerHTML += val.printInfo();
@@ -130,3 +48,4 @@ for (let val of filmSort) {
 
 let s = document.getElementById("sortlikes");
 a.addEventListener("click", sortFilm(), false);
+*/
