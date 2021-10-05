efd7814950086e28fa7cabfd1eefb4b16b779a29
Laura Moldovan <laura.duduma@gmail.com>
2021-10-05 20:58:50 +0200

    ff

--- java/script.js
+++ java/script.js
@@ -1,13 +1,13 @@
 var film = JSON.parse(filme);
 
 function print(y) {
-for (let x of y) {
-  document.getElementById("filme").innerHTML += `<div class="card">
-  <div class="row g-0">
-    <div class="col">
+  for (let x of y) {
+    document.getElementById("filme").innerHTML += `<div class="card">
+      <div class="row g-0">
+      <div class="col">
       <img src="${x.image}" class="img-fluid rounded-start" alt= ${x.title} ${x.year} / >
-    </div>
-    <div class="col">
+      </div>
+      <div class="col">
       <div class="card-body">
         <h5 class="card-title">${x.title}</h5>
         <p class="card-text">${x.description}</p>
@@ -16,39 +16,50 @@ for (let x of y) {
       <p class="myBtn btn btn-primary"><span>&#128077</span> Like</p>
       <p class="qt">${x.like}</p><p>Likes</p>
       </div>
-    </div>
-  </div>
-</div>`;
-}
-function likePlus(x) {
-  y[x].like += 1;
-  document.getElementsByClassName("qt")[x].innerHTML = y[x].like;
-}
+      </div>
+      </div>
+      </div>`;
+  }
 
-var likebut = document.getElementsByClassName("myBtn");
+  function likePlus(x) {
+    y[x].like++;
+    document.getElementsByClassName("qt")[x].innerHTML = y[x].like;
+  }
 
-for (let i = 0; i < likebut.length; i++) {
-  likebut[i].addEventListener("click", function () {
-    likePlus(i);
-  });
-}
-}
+  var likebut = document.getElementsByClassName("myBtn");
 
-print (film);
-
-for (let i of film) {
-  i.like = getElementByClassName("qt").value;
+  for (let i = 0; i < likebut.length; i++) {
+    likebut[i].addEventListener("click", function () {
+      likePlus(i);
+    });
+  }
 }
 
-var filmS = film.sort((a, b) => b.like - a.like);
+print(film);
 
-console.table(filmS);
+var filmSL = film.sort((a, b) => b.like - a.like);
 
-function sort() {
+console.table(filmSL);
+
+function sortedL() {
   document.getElementById("filme").innerHTML = "";
-print(filmS);
+  print(filmSL);
 }
 
-document.getElementById("sortlikes").addEventListener("click", function() {
-  sort();
-  })
+document.getElementById("sortlikes").addEventListener("click", function () {
+  sortedL();
+}); 
+
+var filmSY = film.sort((a, b) => b.year - a.year);
+
+console.table(filmSY);
+
+function sortedY() {
+  document.getElementById("filme").innerHTML = "";
+  print(filmSY);
+}
+
+document.getElementById("sortyear").addEventListener("click", function () {
+  sortedY();
+}); 
+
